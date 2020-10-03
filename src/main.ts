164b3527b8fcee10a9528dfd160109b832af0bbf
Alex <pluscda@gmail.com>
2020-10-03 19:27:15 +0800

    g

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { static as expose } from 'express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(expose('public'));
-  await app.listen(3001);
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Crud  example')
+    .setDescription('NestJS Crud  example')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
 }
 bootstrap();
